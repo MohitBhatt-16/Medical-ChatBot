f5a6d304dd5969f380ee15d77e338ea1a28bba4e
MohitBhatt-16 <mohitbhatt2555@gmail.com>
2024-09-27 18:49:29 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChatBot',
+    version = '0.0.0',
+    author = 'Mohit Bhatt',
+    author_email = 'mohitbhatt2555@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
